23a6fe66b1617ac608e0c2419cf28d49ba0086f0
Sandip Thapa <sandipthapa@cagtu.com>
2022-03-09 17:10:22 +0545

    Head and Image components basic concepts

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,8 +1,19 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import Head from 'next/head'
 
 export default function FirstPost() {
   return (
     <>
+    <Head>
+        <title>First Post</title>
+      </Head>
+    <Image
+    src="/images/profile.jpg" // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt="Your Name"
+  />
       <h1>First Post</h1>
       <h2>
         <Link href="/">
