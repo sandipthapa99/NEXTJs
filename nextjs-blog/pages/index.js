cade00216d44f68bffeae261577da317eaf8ecac
Sandip Thapa <sandipthapa@cagtu.com>
2022-03-10 15:12:24 +0545

    Basic Blog Functionality Finished

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -21,11 +21,7 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello there, Welcome to blog site.</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>Hello there, this site is made using NEXT JS.</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
