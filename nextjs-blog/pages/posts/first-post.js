e4d458ad34b37e17e520b069102a64f5ab7bad89
Sandip Thapa <sandipthapa@cagtu.com>
2022-03-10 10:31:30 +0545

    polishing layout styles

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -10,18 +10,6 @@ export default function FirstPost() {
         <Head>
             <title>First Post</title>
         </Head>
-        <Image
-        src="/images/profile.jpg" // Route of the image file
-        height={144} // Desired size with correct aspect ratio
-        width={144} // Desired size with correct aspect ratio
-        alt="Your Name"
-    />
-        <h1>First Post</h1>
-        <h2>
-            <Link href="/">
-            <a>Back to home</a>
-            </Link>
-        </h2>
     </Layout>
     </>
   )
